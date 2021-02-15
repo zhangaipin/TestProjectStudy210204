05b1914fc93d08d852688d2ebfd490d998447cdb
a51887759 <695883408@qq.com>
2021-02-15 23:36:33 +0800

    2021年2月13日21:55:20 增加UI

--- src/main/java/ui_web/project_ceshiren/CeshirenBasePage.java
+++ src/main/java/ui_web/project_ceshiren/CeshirenBasePage.java
@@ -12,15 +12,15 @@ import ui_web.base.BasePage;
 public class CeshirenBasePage extends BasePage {
     public CeshirenBasePage(String URL) {
         //todo:替换path相对路径，来适合远程Jenkins
-        super(
-                "/usr/local/bin/chromedriver",
-                URL
-        );
-
 //        super(
-//                "src/main/resources/chromedriver.exe",
+//                "/usr/local/bin/chromedriver",
 //                URL
 //        );
+
+        super(
+                "src/main/resources/chromedriver.exe",
+                URL
+        );
 
     }
 
